c71fadfff6cd4d0b0aca1cfb7eaeac104773d032
DedMazzai <ded_mazzai@inbox.ru>
2023-08-01 20:08:41 +0300

    change hibernate on JPA

--- src/main/java/ru/davydenko/spring/springbootrest/dao/EmployeeDAOImpl.java
+++ src/main/java/ru/davydenko/spring/springbootrest/dao/EmployeeDAOImpl.java
@@ -1,8 +1,8 @@
 package ru.davydenko.spring.springbootrest.dao;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
-import org.hibernate.Session;
+import jakarta.persistence.Query;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.davydenko.spring.springbootrest.entity.Employee;
@@ -22,35 +22,24 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public Employee getEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        return session.get(Employee.class, id);
+        return entityManager.find(Employee.class, id);
     }
 
     @Override
     public List<Employee> getAllEmployees() {
-        Session session = entityManager.unwrap(Session.class);
-        TypedQuery<Employee> query = session.createQuery("from Employee", Employee.class);
-        return query.getResultList();
+        Query query = entityManager.createQuery("from Employee");
+        return (List<Employee>) query.getResultList();
     }
 
     @Override
     public void saveEmployee(Employee employee) {
-        Session session = entityManager.unwrap(Session.class);
-        if (employee.getId() == 0) {
-            // Сохраняем новый объект сущности
-            session.persist(employee);
-        } else {
-            // Обновляем существующий объект сущности
-            session.merge(employee);
-        }
+        Employee newEmployee = entityManager.merge(employee);
+        employee.setId(newEmployee.getId());
     }
 
     @Override
     public void deleteEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        Employee employee = session.get(Employee.class, id);
-        if (employee != null) {
-            session.remove(employee);
-        }
+        Query query = entityManager.createQuery("delete from Employee where id = :employeeId");
+        query.setParameter("employeeId", id).executeUpdate();
     }
 }
