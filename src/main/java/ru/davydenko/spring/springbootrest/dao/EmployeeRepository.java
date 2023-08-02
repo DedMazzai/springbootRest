d11989961c27641ead55043d8f2bc22f5b9e9554
DedMazzai <ded_mazzai@inbox.ru>
2023-08-02 10:20:42 +0300

    application with dataRest

--- src/main/java/ru/davydenko/spring/springbootrest/dao/EmployeeRepository.java
+++ src/main/java/ru/davydenko/spring/springbootrest/dao/EmployeeRepository.java
@@ -6,5 +6,4 @@ import ru.davydenko.spring.springbootrest.entity.Employee;
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository <Employee, Integer>{
-    List<Employee> findByName(String name);
 }
